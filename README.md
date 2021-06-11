10b86f18c49431b1015a2cead8d434b6045389e5
toMan <tomasman1979@gmail.com>
2021-06-11 10:46:28 +0200

    work on split_pic function

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# picludo
+# Picludo
+From Latin: Pictura & Secludo - Image & Hide
+**Work in progress:**
+### dependencies
+- NumPy
+- Pillow
+### basic functionality 
+Simple functions to encrypting images. Will include two main functions: \
+**split_pic** use random NumPy array subtraction to make two separate images of random RGB noise. \
+**join_pics** takes two noise images and recovers the original by simple addition. \
+**random_key** noise map generator
+
+### about
+- takes all Pillow supported formats with limitations to RGB and 8bit's per channel
+- works best on lossless bitmaps
+- images cannot be recovered from one noise half 
--- picludo.py
+++ picludo.py
@@ -1,30 +1,48 @@
 from PIL import Image
 import numpy as np
 
-def nahodny_klic(vystup, sirka, vyska):
 
-    pole = np.random.randint(0,256, (vyska,sirka,3))  
-    pole = np.array(pole, dtype=np.uint8)
-    klic = Image.fromarray(pole)
-    klic.save(vystup)
+def random_key(width, height):
+    """NumPy array of given size
 
-# načtení vstupního obrazu jako pole
-obraz1 = Image.open('vstup.jpg')
-vstup = np.asarray(obraz1, dtype=np.uint8)
+    return uint8 data type to keep 8 bit's per channel
+    """
+    np_array = np.random.randint(0, 256, (height, width, 3))
+    return np.array(np_array, dtype=np.uint8)
+
+
+def split_pic(original_image, name_a="out_A.bmp", name_b="out_B.bmp"):
+    """saving two noise images
+
+    
+    """
+    input_img = Image.open(original_image)
+    array_from_input_img = np.asarray(input_img, dtype=np.uint8)
+    random_noise = random_key(input_img.size[0], input_img.size[1])
+    subtracted_array = array_from_input_img - random_noise
+    Image.fromarray(random_noise).save(name_a)  # img from noise map
+    Image.fromarray(subtracted_array).save(name_b)  # img after subtraction
+
+
+
+def join_pics():
+
+    return None
+
 
 # vygenerování a uložení klíče podle rozměru vstupního obrazu
-nahodny_klic('klic.bmp', obraz1.size[0], obraz1.size[1])
+random_key("klic.bmp", obraz1.size[0], obraz1.size[1])
 
 # načtení klíče jako pole
-obraz2 = Image.open('klic.bmp')
+obraz2 = Image.open("klic.bmp")
 klic = np.asarray(obraz2, dtype=np.uint8)
 
 # zamčení obrazu sečtením vstupu a klíče
 zamceno = vstup + klic
 zamceno_img = Image.fromarray(zamceno)
-zamceno_img.save('zamceno.bmp') # uložení zamčeného obrazu
+zamceno_img.save("zamceno.bmp")  # uložení zamčeného obrazu
 
 # odemčení odečtením klíče od zamčeného obrazu
 odemceno = zamceno - klic
 odemceno_img = Image.fromarray(odemceno)
-odemceno_img.save('odemceno.bmp') # uložení obnoveného obrazu
+odemceno_img.save("odemceno.bmp")  # uložení obnoveného obrazu
